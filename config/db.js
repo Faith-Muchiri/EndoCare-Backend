813b0da0feb839897c410db93edb58623facecc6
Faith-Muchiri <faithwambui891@gmail.com>
2024-09-01 01:48:37 +0300

    edit mongo_db connection

--- config/db.js
+++ config/db.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Use environment variable or fallback to default URI
-        const uri = process.env.MONGO_URI || "mongodb+srv://username:password@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority";
-        
-        await mongoose.connect(uri);
-        console.log('MongoDB Connected Successfully...');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected Successfully...')
     } catch (err) {
         console.log(err.message);
         process.exit(1);
