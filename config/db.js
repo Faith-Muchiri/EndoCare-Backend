7e426546a230fa1df418b7ec47c37d21bedcebd5
Faith-Muchiri <faithwambui891@gmail.com>
2024-09-17 09:57:11 +0300

    edit mongodb connection

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected Successfully...')
+        // console.log(process.env.MONGO_URI)
     } catch (err) {
         console.log(err.message);
         process.exit(1);
